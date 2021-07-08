f5702b2be1b4aef31c9be265e7bd0e23502d36b5
Jaime Alvarez <jrralvarez@gmail.com>
2021-07-08 15:31:22 -0400

    have the user input their name and age. added an if statement to check if the age is older than 18

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print('Hello World')
+print("Welcome to my demo game")
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+
+print("Hello ",  name,  " you are ",  age,  " years old.")
+
+if age >= 18:
+    print("You are old enough to play this game!")
+
+    want_to_play = input("Do you want to play? ")
+    if want_to_play == "yes":
+        print("Let's play!")
+    else:
+        print("Come back and play next time!")
+else:
+    print("You aren't old enough to play this game...")
