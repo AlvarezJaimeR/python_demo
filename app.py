3d139553c87a3cbd1fe6d36a457c7f1f8ae0ef12
Jaime Alvarez <jrralvarez@gmail.com>
2021-07-08 15:48:02 -0400

    added a few choices for the user. also added health for the user.

--- app.py
+++ app.py
@@ -4,12 +4,27 @@ age = int(input("What is your age? "))
 
 print("Hello ",  name,  " you are ",  age,  " years old.")
 
+health = 10
+
 if age >= 18:
     print("You are old enough to play this game!")
 
-    want_to_play = input("Do you want to play? ")
+    want_to_play = input("Do you want to play? ").lower()
     if want_to_play == "yes":
         print("Let's play!")
+        print("You are starting with", health, "health.")
+        left_or_right = input("First choice... Left or Right (left/right)? ").lower()
+        if left_or_right == "left":
+            ans = input("Nice, you follow the path and reach a lake... Do you swim across or go around (across/around)? ").lower()
+            if ans == "around":
+                print("You went around and reached the other side of the lake.")
+            elif ans == "across":
+                print("You managed to get across the lake but were bit by a fish and lost 5 health.")
+                health -= 5
+                print("You currently have", health, "health")
+        else:
+            print("You fell down and lost...")
+
     else:
         print("Come back and play next time!")
 else:
